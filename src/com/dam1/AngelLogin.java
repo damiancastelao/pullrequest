ccc2d8ecd684caa9d0c516b86424b3c0d7cb784a
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2021-04-06 11:25:43 +0200

    Creado mi log (#6)

---
+++ src/com/dam1/AngelLogin.java
@@ -0,0 +1,15 @@
+package com.dam1;
+
+/**
+ * Creado por @autor: angel
+ * El  06 de abr. de 2021.
+ * //-encoding utf8 -docencoding utf8 -charset utf8(Para el javadoc)
+ **/
+public class AngelLogin {
+
+    private String userName;
+
+    public void login(){
+        userName="Jose Ángel Sampere Vázquez";
+    }
+}
